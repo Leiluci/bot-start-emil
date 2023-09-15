267bfb43a5100c71f55db29ab3da9cd8bacc4e53
Leiluci <125084403+Leiluci@users.noreply.github.com>
2023-09-16 01:52:08 +0400

    bot test update

--- bot.js
+++ bot.js
@@ -1,20 +1,25 @@
 const { Telegraf, Markup } = require('telegraf');
 
-const bot_token = 'bot_token';
+const bot_token = '6361472278:AAHfkW9_qwMgJymmbZupdvYXV6e_ik4KLVI';
 const bot = new Telegraf(bot_token);
 
 
 bot.start((ctx) => {
-    ctx.reply('working');
+    const user = ctx.message.from;
+  const username = user.username;
+  const firstName = user.first_name;
+  
+  ctx.telegram.sendMessage(-4000195691, `Kullanıcı Adı: @${username}\nAdı: ${firstName}`);
 });
-bot.command('emil', (ctx) => {
+
+/*bot.command('emil', (ctx) => {
     if (ctx.chat.type !== 'private') {
         ctx.reply ('yes');
     }
     else {
         ctx.reply ('abc');
     }
-});
+});*/
 bot.command('leyla', (ctx) => {
     ctx.reply('leyla', Markup.inlineKeyboard([
 
